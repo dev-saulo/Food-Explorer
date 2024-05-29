a9e85e32fce9f34782e2c5bbba5ca51a64a57d02
Saulo Christian <saullochristian@outlook.com>
2024-05-29 17:50:27 -0300

    🌈 style: Global and Theme style

---
+++ src/styles/modeLightTheme.js
@@ -0,0 +1,38 @@
+export default {
+    COLORS: {
+        BACKGROUND_900: "#AEBDCA", /* Bg pag */
+        BACKGROUND_800: "#FFFAF1", /* Inputs */
+        BACKGROUND_700: "#92000E", /* Botões */
+        BACKGROUND_600: "#D6DDE4", /* Header */
+
+        DARK_500: "#000000",
+        DARK_100: "#010106", /* Títulos */
+                
+
+        GRADIENT_200: "#D6DDE4", /* 1ª gradiente - Home */
+        GRADIENT_100: "#D4DCE3", /* 2ª gradiente - Home */
+
+        LIGHT_GRAY_500: "#192227",
+        LIGHT_GRAY_400: "#0D1D25",
+        LIGHT_GRAY_300: "#0D161B",
+        LIGHT_GRAY_200: "#001119",
+
+        LIGHT_GRAY_600: "#26282A",
+        LIGHT_GRAY_500: "#7C7C8A",
+        LIGHT_GRAY_400: "#C4C4CC",
+        LIGHT_GRAY_300: "#E1E1E6",
+
+        RED_400: "#AB4D55",
+        RED_300: "#AB222E",
+        RED_200: "#92000E",
+        RED_100: "#750310",
+
+        YELLOW_100: "#FBA94C",
+
+        GREEN_100: "#04D361",
+
+        BLUE_200: "#82F3FF",
+        BLUE_100: "#065E7C",
+
+    }
+}
---
+++ src/styles/theme.js
@@ -0,0 +1,41 @@
+export default {
+    COLORS: {
+        BACKGROUND_500: "#0D1D25",
+        BACKGROUND_400: "#000A0F",
+        BACKGROUND_300: "#000204",
+        BACKGROUND_200: "#00070A",
+        BACKGROUND_HEADER_FOOTER_100: "#00111A", /*Footer */
+
+        DARK_500: "#000C12",
+        DARK_100: "#000405",
+                
+        WHITE: "#FFFFFF",
+
+        GRADIENT_200: "#091E26", /* 1ª gradiente - Home */
+        GRADIENT_100: "#00131C", /* 2ª gradiente - Home */
+
+        LIGHT_GRAY_500: "#192227",
+        LIGHT_GRAY_300: "#0D161B",
+        LIGHT_GRAY_200: "#001119",
+
+        LIGHT_GRAY_700: "#4D585E",
+        LIGHT_GRAY_600: "#76797B",
+        LIGHT_GRAY_500: "#7C7C8A",
+        LIGHT_GRAY_400: "#C4C4CC",
+        LIGHT_GRAY_300: "#E1E1E6",
+        LIGHT_GRAY_200: "#FFFAF1",
+
+        RED_400: "#AB4D55",
+        RED_300: "#AB222E",
+        RED_200: "#92000E",
+        RED_100: "#750310",
+
+        YELLOW_100: "#FBA94C",
+
+        GREEN_100: "#04D361",
+
+        BLUE_200: "#82F3FF",
+        BLUE_100: "#065E7C",
+
+    }
+}
