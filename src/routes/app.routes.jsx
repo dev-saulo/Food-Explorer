35222beea1fd329392e5de1b186800ec843d1ae4
Saulo Christian <saullochristian@outlook.com>
2024-06-05 10:07:17 -0300

    ✨ feat

---
+++ src/routes/app.routes.jsx
@@ -0,0 +1,23 @@
+import { Routes, Route } from "react-router-dom";
+
+import { Home } from "../pages/Home";
+import { CreateDish } from "../pages/AddDish";
+import { EditDish } from "../pages/Edit";
+import { Profile } from "../pages/Profile";
+import { Details } from "../pages/Details";
+import { Card } from "../pages/Card";
+import { Orders } from "../pages/OrdersHistory";
+
+export function AppRoutes() {
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/AddDish" element={<CreateDish />} />
+            <Route path="/edit/:id" element={<EditDish />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/card" element={<Card />} />
+            <Route path="/OrdersHistory" element={<Orders />} />
+        </Routes>
+    )
+}
